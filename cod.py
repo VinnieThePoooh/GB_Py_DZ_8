f3a85b4999be811102f63f947e504a43801f9007
Керим Гучигов <forsyte12316gmail.com@MacBook-Air-Kerim.local>
2023-07-05 11:42:25 +0300

    создана программа с возможностью добавления, чтения и изменения данных в отдельном файле

--- cod.py
+++ cod.py
@@ -11,13 +11,27 @@ def print_info(file):
 def write_info(file):
     name = input('Введите имя контакта: ')
     number = input('Введите номер контакта: ')
-    with open(file, 'r', encoding='utf-8') as f:
-        lines = len(f.readlines())
-    with open(file, 'w', encoding='utf-8') as f:
-        f.write(str(lines) + ') ' + name + ' - ' + number + '\n')
+    with open(file, 'a', encoding='utf-8') as f:
+        f.write(name + ' - ' + number + '\n')
     print('Готово')
 
-
+def change_info(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        counter = 1
+        for i in f:
+            print(str(counter) + ')'+' ' +i)
+            counter+=1
+    user_choice=int(input('Выберите номер контакта, в котором хотите внести изменения: '))
+    with open(file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        lines.pop(user_choice-1)
+    print(lines)
+    name = input('Введите имя контакта: ')
+    number = input('Введите номер контакта: ')
+    with open(file, 'a', encoding='utf-8') as f:
+        f.write(name + ' - ' + number + '\n')
+    print('Готово')
+       
 def good_bye():
 
     print('До свидания')
@@ -31,8 +45,9 @@ def main():
         print('Чтобы закрыть книгу контактов, нажмите 0')
         print('Чтобы посмотреть книгу контактов, нажмите 1')
         print('Чтобы записать новый контакт, нажмите 2')
+        print('Чтобы изменить существующий контакт, нажмите 3')
         user_choice = int(input())
-        operation = {0: good_bye, 1: print_info, 2: write_info}
+        operation = {0: good_bye, 1: print_info, 2: write_info, 3: change_info }
         if user_choice == 0:
             operation[user_choice]()
         else:
